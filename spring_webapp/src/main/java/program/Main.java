c8d775e52c508de5fdee9c0d07537896cc026670
novakvova <novakvova@gmail>
2023-03-27 15:37:11 +0300

    Begin workign auth begin

--- spring_webapp/src/main/java/program/Main.java
+++ spring_webapp/src/main/java/program/Main.java
@@ -1,5 +1,10 @@
 package program;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +17,8 @@ import program.storage.StorageService;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
+@SecurityScheme(name = "vovan-api", scheme = "bearer", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
+@OpenAPIDefinition(info = @Info(title = "Shop API", version = "2.0", description = "List Products"))
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello String Boot!");
