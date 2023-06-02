79023ce04ad2b4bce2899c2f631a421de2458bec
novakvova <novakvova@gmail>
2023-06-02 20:45:07 +0300

    Working interceptor test

--- spring_webapp/target/generated-sources/annotations/program/mapper/CategoryMapperImpl.java
+++ spring_webapp/target/generated-sources/annotations/program/mapper/CategoryMapperImpl.java
@@ -10,7 +10,7 @@ import program.entities.CategoryEntity;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-04-05T15:50:15+0300",
+    date = "2023-06-02T20:35:48+0300",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 19.0.2 (Oracle Corporation)"
 )
 @Component
